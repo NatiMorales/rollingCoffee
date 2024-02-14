c4a4eefd43bd169b08ead675b323d1519d55aa47
Natalia Morales <natii.vmorales98@gmail.com>
2024-02-14 10:26:51 -0300

    agregamos las rutas del protectos en App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,19 @@ import Error404 from "./components/pages/Error404";
 import Administrador from "./components/pages/Administrador";
 import Inicio from "./components/pages/Inicio";
 import FormularioProducto from './components/pages/producto/FormularioProducto';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Menu></Menu>
-      {/* <Inicio></Inicio> */}
-      {/* <Administrador></Administrador> */}
-      {/* <FormularioProducto></FormularioProducto> */}
-      <Error404></Error404>
+      <Routes>
+        <Route exact path="/" element={<Inicio></Inicio>}></Route>
+        <Route exact path="/administrador" element={<Administrador></Administrador>}></Route>
+        <Route path='*' element={<Error404></Error404>}></Route>
+      </Routes>
       <Footer></Footer>
-    </>
+    </BrowserRouter>
   );
 }
 
